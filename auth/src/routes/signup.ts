5aaf8b99925536ea76f1bf34b6f34abe61601ccb
a.smosljajev <a.smosljajev@gmail.com>
2022-03-07 14:28:57 +0100

    validation and error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -11,9 +13,19 @@ router.post('/api/users/signup', [
     .trim().isLength({min:4, max:20})
     .withMessage('Password must be valid!')
 ],
-(req: Request, res: Response) => {
+async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        throw new RequestValidationError(errors.array());
+    }
+
     const { email, password } = req.body;
-    res.send('signed up!');
+
+    console.log('creating a user...');
+    throw new DatabaseConnectionError();
+    
+    res.send({});
 });
 
 export {router as signupRouter };
