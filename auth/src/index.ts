7441759a9a5d16510d6d9312cead414cc9e2f506
a.smosljajev <a.smosljajev@gmail.com>
2022-03-07 16:22:30 +0100

    added mongoose connection to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -23,7 +25,18 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('connected to mongodb');
+    }
+    catch(err){
+        console.log(err)
+    }
+}
 
 app.listen(3000, () => {
     console.log('Listening on port 3000 !')
 })
+
+start();
