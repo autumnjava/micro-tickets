0f1f786638705bf4361b9b9a909825c5ed81f146
a.smosljajev <a.smosljajev@gmail.com>
2022-03-09 10:18:23 +0100

    added simple token generation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -31,6 +33,18 @@ async (req: Request, res: Response) => {
 
     const user = User.build({email, password});
     await user.save();
+
+    // generate JWT
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, 'asdfg');
+
+    // store it on session object
+    // req.session.jwt = userJwt; // because of typescript have to do as below:
+    req.session = {
+        jwt: userJwt
+    };
 
     res.status(201).send(user);
     
