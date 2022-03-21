2889ae1f0ee037e6425a677b5954e6433ba3ba96
a.smosljajev <a.smosljajev@gmail.com>
2022-03-21 15:06:01 +0100

    small refactoring

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,12 @@
 import express, {Request, Response } from 'express';
 
+import { currentUser } from '../middlewares/current-user';
+
+
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-    res.send('current user!');
-
+router.get('/api/users/currentuser', currentUser, (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
 });
 
 export {router as currentUserRouter };
