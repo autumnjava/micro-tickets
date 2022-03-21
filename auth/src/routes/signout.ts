b8d51ed543ab06bd83d5b0a8c1d709df272d0da1
a.smosljajev <a.smosljajev@gmail.com>
2022-03-21 15:06:09 +0100

    logout route handler

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,10 @@ import express, {Request, Response } from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req: Request, res: Response) => {
-    res.send('signed out!');
+    req.session = null;
+
+
+    res.send({});
 
 });
 
