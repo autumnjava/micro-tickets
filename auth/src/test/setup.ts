56e57f68483054b57b0a86408ce43ba6760205b5
a.smosljajev <a.smosljajev@gmail.com>
2022-03-24 12:48:17 +0100

    helper function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,12 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+
+declare global {
+    var getAuthCookie: () => Promise<string[]>
+}
 
 let mongo: any;
 
@@ -25,3 +31,20 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+
+global.getAuthCookie = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const res = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email, password
+        })
+        .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    return cookie;
+}
