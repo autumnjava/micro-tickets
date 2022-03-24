a9220c47d79fe10e88dc4d74e8cbb13f645123cf
a.smosljajev <a.smosljajev@gmail.com>
2022-03-24 16:59:33 +0100

    basic signup form

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,17 @@
+export default () => {
+    return (
+        <form>
+            <h1>Sign up</h1>
+            <div className="form-group">
+                <label>Email Adress</label>
+                <input type="email" className="form-control" />
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" />
+            </div>
+            <button className="btn btn-primary">Sign up</button>
+        </form>
+
+    )
+}
