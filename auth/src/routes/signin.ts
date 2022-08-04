fb39ec8422440fbfc3d760d4986c58eccd900f27
Alexander S <70532114+autumnjava@users.noreply.github.com>
2022-08-04 14:08:28 +0200

    Update signin.ts

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -42,15 +42,13 @@ async (req: Request, res: Response) => {
     process.env.JWT_KEY!
     );
 
-    // // store it on session object
-    // // req.session.jwt = userJwt; // because of typescript have to do as below:
-    // req.session = {
-    //     jwt: userJwt
-    // };
-
-    // res.status(200).send(existingUser);
-    res.send({})
-
+    return res
+    .cookie("access_token", userJwt, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    })
+    .status(200)
+    .send(existingUser);
 });
 
 export {router as signinRouter };
