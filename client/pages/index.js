bb01718a55bd07e4bf3d9beb8f3ded685a76dfd1
Alexander S <70532114+autumnjava@users.noreply.github.com>
2022-08-04 16:03:56 +0200

    added build-client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,22 +1,14 @@
-import axios from 'axios';
+import buildClient from "../api/build-client";
 
 const LadingPage =  ({ currentUser }) => {
   console.log(currentUser, 'current user mhm');
     return <h1> landing page </h1>;
 }
 
-LadingPage.getInitialProps = async ({ req }) => {
-  if(typeof window==='undefined'){
-    // we are on the server yo
-    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-      headers: req.headers,
-    });
-    return data;
-  } else {
-    console.log('we get here or nah?');
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
+LadingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
 };
 
 export default LadingPage;
