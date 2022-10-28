228287cec0df266027f38774bcfb5617a345b7cd
Alexander S <70532114+autumnjava@users.noreply.github.com>
2022-10-28 13:06:30 +0200

    publish event on ticket created

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { requireAuth, validateRequest } from '@gunit/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -21,10 +23,16 @@ router.post(
     const ticket = Ticket.build({
       title,
       price,
-      userId: req.currentUser!.id
+      userId: req.currentUser!.id,
     });
 
     await ticket.save();
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
