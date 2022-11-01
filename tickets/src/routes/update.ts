ea9818d77a1ebc95a1d824e4f5d3e4bd56086269
Alexander S <70532114+autumnjava@users.noreply.github.com>
2022-11-01 18:50:11 +0100

    Update update.ts

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -41,6 +41,7 @@ router.put(
     }
 
     ticket.set({ title: req.body.title, price: req.body.price });
+
     await ticket.save();
     
     new TicketUpdatedPublisher(natsWrapper.client).publish({
@@ -50,7 +51,7 @@ router.put(
       userId: ticket.userId,
     });
 
-    res.status(200).send(ticket);
+    res.send(ticket);
   }
 );
 
