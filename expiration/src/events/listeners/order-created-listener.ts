a7dad5a7f47a666bfc2b2a12aaeaf45bfbfcf7d9
Alexander S <70532114+autumnjava@users.noreply.github.com>
2023-04-04 14:42:45 +0200

    fix queue bug...

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -8,12 +8,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log(delay, 'delay ms');
+    
     await expirationQueue.add(
         {
           orderId: data.id,
         },
         {
-          delay: 10000,
+          delay,
         }
       );
 
