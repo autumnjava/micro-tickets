fad46b64d63c00989712adb83cbb77e5031bfc70
Alexander S <70532114+autumnjava@users.noreply.github.com>
2023-05-27 20:17:13 +0200

    payment model and some tests

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import { Order } from '../../models/order';
 import { OrderStatus } from '@gunit/common';
 import { stripe } from '../../stripe';
+import { Payment } from '../../models/payment';
 
 // jest.mock('../../stripe');
 
@@ -91,4 +92,11 @@ it('returns a 204 with valid inputs', async () => {
   });
 
   expect(stripeCharge).toBeDefined();
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+
+  expect(payment).not.toBeNull();
 });
